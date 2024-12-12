b249e0eac1065635f3cacea6a5b4e56952aa75aa
Faris Riza M <13223079@std.stei.itb.ac.id>
2024-12-12 13:44:34 +0700

    Update Multiplier16Bit.vhd

--- Multiplier16Bit.vhd
+++ Multiplier16Bit.vhd
@@ -1,8 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
+use IEEE.NUMERIC_STD.ALL; 
 entity Multiplier16Bit is
     Port (
         A : in  STD_LOGIC_VECTOR(15 downto 0);
@@ -15,6 +13,7 @@ architecture Behavioral of Multiplier16Bit is
 begin
     process(A, B)
     begin
+        -- Konversi A dan B ke tipe unsigned sebelum dikalikan
         Result <= std_logic_vector(unsigned(A) * unsigned(B));
     end process;
 end Behavioral;
