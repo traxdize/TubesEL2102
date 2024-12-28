ed7abaf07517bb88bd2f716312cd6a8a4decdf09
Faris Riza M <13223079@std.stei.itb.ac.id>
2024-12-28 15:22:23 +0700

    Update Multiplier16Bit.vhd

--- Multiplier16Bit.vhd
+++ Multiplier16Bit.vhd
@@ -4,18 +4,17 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Multiplier16Bit is
     Port (
-        A : in  STD_LOGIC_VECTOR(15 downto 0); -- Input operand 1
-        B : in  STD_LOGIC_VECTOR(15 downto 0); -- Input operand 2
-        Result : out STD_LOGIC_VECTOR(30 downto 0) -- Output result (31-bit)
+        A : in  STD_LOGIC_VECTOR(15 downto 0); -- Input 1 (16 Bit)
+        B : in  STD_LOGIC_VECTOR(15 downto 0); -- Input 2 (16 Bit)
+        Result : out STD_LOGIC_VECTOR(30 downto 0) -- Output (31-bit)
     );
 end Multiplier16Bit;
-
+    
 architecture Behavioral of Multiplier16Bit is
     -- Signals for signed conversion and multiplication
     signal signed_A : SIGNED(15 downto 0);
     signal signed_B : SIGNED(15 downto 0);
     signal signed_Product : SIGNED(30 downto 0);
-
 begin
     -- Convert inputs to signed
     signed_A <= SIGNED(A);
@@ -26,6 +25,5 @@ begin
 
     -- Assign result to output
     Result <= STD_LOGIC_VECTOR(signed_Product);
-
 end Behavioral;
 
