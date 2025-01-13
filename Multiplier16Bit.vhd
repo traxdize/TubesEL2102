e9f2e979020a60432b17b7d63c2df0d6f5aaebc0
Faris Riza M <13223079@std.stei.itb.ac.id>
2025-01-13 10:04:15 +0700

    Update Multiplier16Bit.vhd

--- Multiplier16Bit.vhd
+++ Multiplier16Bit.vhd
@@ -4,25 +4,14 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Multiplier16Bit is
     Port ( 
-        a       : in  STD_LOGIC_VECTOR(15 downto 0);    -- Input A (16-bit)
-        b       : in  STD_LOGIC_VECTOR(15 downto 0);    -- Input B (16-bit)
-        result  : out STD_LOGIC_VECTOR(31 downto 0)     -- Output hasil (32-bit)
+        a : in signed(15 downto 0);
+        b   : in signed(15 downto 0);
+        product     : out signed(31 downto 0)
     );
 end Multiplier16Bit;
 
 architecture Behavioral of Multiplier16Bit is
-    signal a_signed   : SIGNED(15 downto 0);            -- Sinyal internal bertipe signed untuk A
-    signal b_signed   : SIGNED(15 downto 0);            -- Sinyal internal bertipe signed untuk B
-    signal mult_result: SIGNED(31 downto 0);            -- Hasil perkalian internal
 begin
-    -- Mengubah input vector menjadi signed
-    a_signed <= SIGNED(a);
-    b_signed <= SIGNED(b);
-    
-    -- Melakukan perkalian bertanda (kombinasi)
-    mult_result <= a_signed * b_signed;
-    
-    -- Mengonversi hasil kembali ke std_logic_vector untuk output
-    result <= STD_LOGIC_VECTOR(mult_result);
-
+    -- Perform signed multiplication
+    product <= a * b;
 end Behavioral;
